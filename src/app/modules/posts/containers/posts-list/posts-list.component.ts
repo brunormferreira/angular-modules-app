6835e72b8a851fb0a2fa66e8c4966fe79e68cf55
Bruno Ramires <brunormferreira@hotmail.com>
2021-12-22 11:33:06 -0300

    add(feat): posts-list container integrated

--- src/app/modules/posts/containers/posts-list/posts-list.component.ts
+++ src/app/modules/posts/containers/posts-list/posts-list.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { IPost } from 'src/app/core/models/post.model';
+import { PostsService } from '../../../../core/services/posts/posts.service';
+
+import { tap, delay } from 'rxjs';
 
 @Component({
   selector: 'app-posts-list',
   templateUrl: './posts-list.component.html',
-  styleUrls: ['./posts-list.component.scss']
+  styleUrls: ['./posts-list.component.scss'],
 })
 export class PostsListComponent implements OnInit {
+  posts: IPost[] = [];
+  isLoading: boolean = false;
 
-  constructor() { }
+  constructor(private postsService: PostsService) {}
 
   ngOnInit(): void {
-  }
+    this.isLoading = true;
 
+    this.postsService
+      .getPosts()
+      .pipe(
+        delay(1000),
+        tap(() => (this.isLoading = false))
+      )
+      .subscribe((posts: IPost[]) => (this.posts = posts));
+  }
 }
