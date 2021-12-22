d5f784cb06a8b3fe7358e25f4265a0d5c3eef504
Bruno Ramires <brunormferreira@hotmail.com>
2021-12-22 11:32:32 -0300

    add(feat): spinner component and his module

---
+++ src/app/shared/components/spinner/spinner.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-spinner',
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.scss'],
+})
+export class SpinnerComponent implements OnInit {
+  @Input() isLoading: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
