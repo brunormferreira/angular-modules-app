93caedd12af5de4d3783898fbf54f1c3ca7a3883
Bruno Ramires <brunormferreira@hotmail.com>
2021-12-22 11:33:52 -0300

    add(feat): details container with edit ngModel

--- src/app/modules/posts/containers/posts-details/posts-details.component.ts
+++ src/app/modules/posts/containers/posts-details/posts-details.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { IPost } from 'src/app/core/models/post.model';
+import { PostsService } from '../../../../core/services/posts/posts.service';
 
+import { tap, delay } from 'rxjs';
 @Component({
   selector: 'app-posts-details',
   templateUrl: './posts-details.component.html',
-  styleUrls: ['./posts-details.component.scss']
+  styleUrls: ['./posts-details.component.scss'],
 })
 export class PostsDetailsComponent implements OnInit {
+  @Input() post!: IPost;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
+    this.loadPostById();
   }
 
+  loadPostById(): void {
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!);
+    this.postsService
+      .getPostById(id)
+      .subscribe((post: IPost) => (this.post = post));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    if (this.post) {
+      this.postsService
+        .updatePost(this.post)
+        .pipe(
+          delay(1000),
+          tap((post: IPost) => console.log(':: UPDATED TITLE POST ::', post))
+        )
+        .subscribe(() => this.goBack());
+    }
+  }
 }
