b2528670b6353af35a5d4c6bcc589e5a48596c12
Bruno Ramires <brunormferreira@hotmail.com>
2021-12-22 11:55:24 -0300

    add(feat): handleError on posts service

--- src/app/core/services/posts/posts.service.ts
+++ src/app/core/services/posts/posts.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
 import { IPost } from '../../models/post.model';
 import { IComment } from '../../models/comment.model';
-import { Observable } from 'rxjs';
+import { Observable, catchError } from 'rxjs';
 import { environment } from '../../../../environments/environment';
+import { throwError } from 'rxjs';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -19,36 +24,55 @@ export class PostsService {
   constructor(private httpClient: HttpClient) {}
 
   public getPosts(): Observable<IPost[]> {
-    return this.httpClient.get<IPost[]>(`${this.contextUrl}`);
+    return this.httpClient
+      .get<IPost[]>(`${this.contextUrl}`)
+      .pipe(catchError(this.handleError));
   }
 
   public getPostById(id: number): Observable<IPost> {
-    return this.httpClient.get<IPost>(`${this.contextUrl}/${id}`);
+    return this.httpClient
+      .get<IPost>(`${this.contextUrl}/${id}`)
+      .pipe(catchError(this.handleError));
   }
 
   public getCommentsByPostId(postId: number): Observable<IComment[]> {
-    return this.httpClient.get<IComment[]>(
-      `${this.contextUrl}/${postId}/comments`
-    );
+    return this.httpClient
+      .get<IComment[]>(`${this.contextUrl}/${postId}/comments`)
+      .pipe(catchError(this.handleError));
   }
 
   public createPost(payload: IPost): Observable<IPost> {
-    return this.httpClient.post<IPost>(
-      `${this.contextUrl}`,
-      payload,
-      httpOptions
-    );
+    return this.httpClient
+      .post<IPost>(`${this.contextUrl}`, payload, httpOptions)
+      .pipe(catchError(this.handleError));
   }
 
   public updatePost(payload: IPost): Observable<IPost> {
-    return this.httpClient.put<IPost>(
-      `${this.contextUrl}/${payload.id}`,
-      payload,
-      httpOptions
-    );
+    return this.httpClient
+      .put<IPost>(`${this.contextUrl}/${payload.id}`, payload, httpOptions)
+      .pipe(catchError(this.handleError));
   }
 
   public deletePost(id: number): Observable<void> {
-    return this.httpClient.delete<void>(`${this.contextUrl}/${id}`);
+    return this.httpClient
+      .delete<void>(`${this.contextUrl}/${id}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknow error occurred!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(() => new Error(errorMessage));
+    }
+
+    switch (errorRes.error.error.message) {
+      case 'TITLE_EXISTS':
+        errorMessage = 'This title exists already';
+        break;
+      case 'INVALID_TITLE':
+        errorMessage = 'This title is not valid.';
+        break;
+    }
+    return throwError(() => new Error(errorMessage));
   }
 }
