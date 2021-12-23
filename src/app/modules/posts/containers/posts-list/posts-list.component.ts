aab78f98177b6505e0b51e2355d2446af73fb478
Bruno Ramires <brunormferreira@hotmail.com>
2021-12-23 12:08:49 -0300

    add(feat): creating component programatically

--- src/app/modules/posts/containers/posts-list/posts-list.component.ts
+++ src/app/modules/posts/containers/posts-list/posts-list.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { IPost } from '../../../../core/models/post.model';
 import { IRoutes } from '../../../../core/models/routes.model';
 import { PostsService } from '../../../../core/services/posts/posts.service';
 
-import { tap, delay } from 'rxjs';
+import { tap, delay, Subscription } from 'rxjs';
+import { PlaceholderDirective } from '../../../../shared/directives/placeholder.directive';
+import { AlertComponent } from '../../../../shared/components/alert/alert.component';
 
 @Component({
   selector: 'posts-list',
@@ -13,6 +15,11 @@ import { tap, delay } from 'rxjs';
 export class PostsListComponent implements OnInit {
   posts: IPost[] = [];
   isLoading: boolean = true;
+
+  @ViewChild(PlaceholderDirective)
+  alertHost!: PlaceholderDirective;
+
+  private closeSub!: Subscription;
 
   routesValue: IRoutes[] = [
     {
@@ -32,7 +39,12 @@ export class PostsListComponent implements OnInit {
         delay(1000),
         tap(() => (this.isLoading = false))
       )
-      .subscribe((posts: IPost[]) => (this.posts = posts));
+      .subscribe({
+        next: (posts: IPost[]) => (this.posts = posts),
+        error: (errorMessage: string) => {
+          this.showErrorAlert(errorMessage);
+        },
+      });
   }
 
   onDeletePost(post: IPost): void {
@@ -40,11 +52,26 @@ export class PostsListComponent implements OnInit {
       'Are you sure you want to delete this post?' + '\n' + post.title
     );
 
-    if (isValid) {
+    if (isValid && post.id) {
       this.postsService
-        .deletePost(post.id!)
+        .deletePost(post.id)
         .pipe(tap(() => console.log(':: DELETED POST ::', post)))
         .subscribe(() => alert('Post deleted with success!'));
+    }
+  }
+
+  private showErrorAlert(message: string) {
+    if (this.alertHost) {
+      const hostViewContainerRef = this.alertHost.viewContainerRef;
+      hostViewContainerRef.clear();
+
+      const componentRef = hostViewContainerRef.createComponent(AlertComponent);
+
+      componentRef.instance.message = message;
+      this.closeSub = componentRef.instance.close.subscribe(() => {
+        this.closeSub.unsubscribe();
+        hostViewContainerRef.clear();
+      });
     }
   }
 }
