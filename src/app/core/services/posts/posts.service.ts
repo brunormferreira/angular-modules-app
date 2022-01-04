25de40c4867a69728a2b4dcc4f61a3a8d7bac5b3
Bruno Ramires <brunormferreira@hotmail.com>
2022-01-04 12:22:05 -0300

    add(feat): messages on post service to handle logs

--- src/app/core/services/posts/posts.service.ts
+++ src/app/core/services/posts/posts.service.ts
@@ -8,7 +8,8 @@ import { IPost } from '../../models/post.model';
 import { IComment } from '../../models/comment.model';
 import { Observable, catchError } from 'rxjs';
 import { environment } from '../../../../environments/environment';
-import { throwError } from 'rxjs';
+import { throwError, tap } from 'rxjs';
+import { MessageService } from '../messages/messages.service';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -21,42 +22,57 @@ const httpOptions = {
 export class PostsService {
   private contextUrl = `${environment.apiUrl}/posts`;
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private messageService: MessageService
+  ) {}
 
   public getPosts(): Observable<IPost[]> {
-    return this.httpClient
-      .get<IPost[]>(`${this.contextUrl}`)
-      .pipe(catchError(this.handleError));
+    return this.httpClient.get<IPost[]>(`${this.contextUrl}`).pipe(
+      tap((_) => this.log('fetched posts')),
+      catchError(this.handleError)
+    );
   }
 
   public getPostById(id: number): Observable<IPost> {
-    return this.httpClient
-      .get<IPost>(`${this.contextUrl}/${id}`)
-      .pipe(catchError(this.handleError));
+    return this.httpClient.get<IPost>(`${this.contextUrl}/${id}`).pipe(
+      tap((_) => this.log(`fetched post id=${id}`)),
+      catchError(this.handleError)
+    );
   }
 
   public getCommentsByPostId(postId: number): Observable<IComment[]> {
     return this.httpClient
       .get<IComment[]>(`${this.contextUrl}/${postId}/comments`)
-      .pipe(catchError(this.handleError));
+      .pipe(
+        tap((_) => this.log(`fetched comments by post id=${postId}`)),
+        catchError(this.handleError)
+      );
   }
 
   public createPost(payload: IPost): Observable<IPost> {
     return this.httpClient
       .post<IPost>(`${this.contextUrl}`, payload, httpOptions)
-      .pipe(catchError(this.handleError));
+      .pipe(
+        tap((newPost: IPost) => this.log(`added new post id=${newPost.id}`)),
+        catchError(this.handleError)
+      );
   }
 
   public updatePost(payload: IPost): Observable<IPost> {
     return this.httpClient
       .put<IPost>(`${this.contextUrl}/${payload.id}`, payload, httpOptions)
-      .pipe(catchError(this.handleError));
+      .pipe(
+        tap((_) => this.log(`updated post id=${payload.id}`)),
+        catchError(this.handleError)
+      );
   }
 
   public deletePost(id: number): Observable<void> {
-    return this.httpClient
-      .delete<void>(`${this.contextUrl}/${id}`)
-      .pipe(catchError(this.handleError));
+    return this.httpClient.delete<void>(`${this.contextUrl}/${id}`).pipe(
+      tap((_) => this.log(`deleted post id=${id}`)),
+      catchError(this.handleError)
+    );
   }
 
   private handleError(errorRes: HttpErrorResponse) {
@@ -74,5 +90,9 @@ export class PostsService {
         break;
     }
     return throwError(() => new Error(errorMessage));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`PostsService: ${message}`);
   }
 }
